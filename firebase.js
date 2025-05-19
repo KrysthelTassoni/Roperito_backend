35e7464cf5abfe523ba893a11d06d582d673464d
unknown <riveros.bryan@gmail.com>
2025-05-19 00:25:33 -0400

    change data in firebase.js 2

--- firebase.js
+++ firebase.js
@@ -1,16 +1,26 @@
-// firebase.js
 import { initializeApp, cert } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
+require("dotenv").config(); // o import 'dotenv/config' si usas ESM
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT; // Reemplaza con la ruta a tu archivo de cuenta de servicio
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 // Inicializa la aplicación Firebase
 const app = initializeApp({
   credential: cert(serviceAccount),
-  storageBucket: "roperito-4d180.firebasestorage.app", // Reemplaza con el nombre de tu bucket de Firebase Storage
+  storageBucket: "roperito-4d180.appspot.com", // Corrección: debe terminar en .appspot.com
 });
 
-// Exporta el almacenamiento de Firebase
 const bucket = getStorage(app).bucket();
-
 export { bucket };
